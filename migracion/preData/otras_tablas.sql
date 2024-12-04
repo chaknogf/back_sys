4b5f5026ee3001b197389528c6ab4c3cc0f85713
chaknogf <chaknogf@gmail.com>
2024-12-04 12:36:53 -0600

    feat: migracion de medicos, usuarios y constancias de nacimiento

--- migracion/preData/otras_tablas.sql
+++ migracion/preData/otras_tablas.sql
@@ -1,47 +1,98 @@
--- Actualizar valores nulos o incorrectos en las tablas existentes
+CREATE TABLE nuevacitas (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    paciente_id INT DEFAULT NULL,
+    expediente INT DEFAULT NULL,
+    especialidad INT DEFAULT NULL,
+    fecha DATE DEFAULT NULL,
+    nota TEXT DEFAULT NULL,
+    tipo INT DEFAULT NULL,
+    lab INT DEFAULT NULL,
+    fecha_lab DATE DEFAULT NULL,
+    created_at DATETIME DEFAULT NULL,
+    updated_at DATETIME DEFAULT NULL,
+    created_by VARCHAR(8) DEFAULT NULL
+) ENGINE = InnoDB CHARSET = utf8mb4;
+
+INSERT INTO
+    nuevacitas (
+        expediente,
+        especialidad,
+        fecha,
+        nota,
+        tipo,
+        created_at,
+        updated_at,
+        created_by,
+        lab,
+        fecha_lab
+    )
+SELECT
+    expediente,
+    especialidad,
+    fecha,
+    nota,
+    tipo,
+    created_at,
+    updated_at,
+    created_by,
+    lab,
+    fecha_lab
+FROM citas;
+
+DELETE FROM nuevacitas WHERE expediente IS NULL;
+
+UPDATE nuevacitas
+SET
+    especialidad = CASE
+        WHEN especialidad = 0 THEN NULL
+        WHEN especialidad = 9 THEN 1
+        ELSE especialidad
+    END
+WHERE
+    especialidad IN (0, 9);
+
+UPDATE nuevacitas
+SET
+    tipo = CASE
+        WHEN tipo = 0 THEN 1
+        WHEN tipo = 9 THEN 1
+        WHEN tipo = 8 THEN 3
+        ELSE tipo
+    END
+WHERE
+    tipo IN (0, 9);
+
+CREATE INDEX idx_nuevacitas_expediente ON nuevacitas (expediente);
+
+CREATE INDEX idx_nuevaexpediente_expediente ON nuevaexpediente (expediente);
+
+UPDATE nuevacitas nc
+JOIN nuevaexpediente ne ON nc.expediente = ne.expediente
+SET
+    nc.paciente_id = ne.paciente_id
+WHERE
+    nc.expediente IS NOT NULL;
+
 UPDATE medicos SET especialidad = NULL WHERE especialidad = 0;
 
 UPDATE usuarios SET rol = 4 WHERE rol = 0;
 
--- DELETE FROM medicos WHERE colegiado = 0;
+UPDATE usuarios SET dpi = NULL WHERE dpi = 0;
 
--- Crear la nueva tabla
+DELETE FROM medicos WHERE colegiado = 0;
+
 CREATE TABLE nuevausuario (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(10) UNIQUE NOT NULL,
-    nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    contraseña VARCHAR(255) NOT NULL,
+    nombre VARCHAR(100) DEFAULT NULL,
+    email VARCHAR(100) DEFAULT NULL,
+    contraseña VARCHAR(255) DEFAULT NULL,
     dpi BIGINT UNIQUE DEFAULT NULL,
     rol INT NOT NULL,
     created_at DATETIME,
     updated_at DATETIME
 ) ENGINE = InnoDB CHARSET = utf8mb4;
 
--- Insertar datos desde la tabla 'usuarios' en la nueva tabla
-INSERT INTO
-    nuevausuario (
-        username,
-        nombre,
-        email,
-        contraseña,
-        dpi,
-        rol,
-        created_at,
-        updated_at
-    )
-SELECT
-    username,
-    name AS nombre,
-    email,
-    password AS contraseña,
-    dpi,
-    rol,
-    created_at,
-    updated_at
-FROM usuarios;
-
--- Crear la nueva tabla 'nuevamedicos'
 CREATE TABLE nuevamedicos (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Hacemos que sea autoincremental
     colegiado INT NOT NULL,
@@ -54,30 +105,7 @@ CREATE TABLE nuevamedicos (
     updated_at DATETIME
 ) ENGINE = InnoDB CHARSET = utf8mb4;
 
--- Insertar datos desde la tabla 'medicos' en 'nuevamedicos'
-INSERT INTO
-    nuevamedicos (
-        colegiado,
-        nombre,
-        dpi,
-        especialidad,
-        pasaporte,
-        sexo,
-        created_at,
-        updated_at
-    )
-SELECT
-    colegiado,
-    name AS nombre,
-    dpi,
-    especialidad,
-    pasaporte,
-    sexo,
-    created_at,
-    updated_at
-FROM medicos;
-
-CREATE TABLE nueva_rn (
+CREATE TABLE nuevarn (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     fecha DATE DEFAULT NULL,
     cor INT DEFAULT NULL,
@@ -91,7 +119,7 @@ CREATE TABLE nueva_rn (
     passport VARCHAR(30) DEFAULT NULL,
     libro INT DEFAULT NULL,
     folio INT DEFAULT NULL,
-    partida INT DEFAULT NULL,
+    partida VARCHAR(20) DEFAULT NULL,
     depto INT DEFAULT NULL,
     muni INT DEFAULT NULL,
     edad INT DEFAULT NULL,
@@ -117,10 +145,52 @@ CREATE TABLE nueva_rn (
     updated_at DATETIME
 ) ENGINE = InnoDB CHARSET = utf8mb4;
 
--- Inserción en la nueva tabla
 INSERT INTO
-    nueva_rn (
-        id,
+    nuevausuario (
+        username,
+        nombre,
+        email,
+        contraseña,
+        dpi,
+        rol,
+        created_at,
+        updated_at
+    )
+SELECT
+    username,
+    name AS nombre,
+    email,
+    password AS contraseña,
+    dpi,
+    rol,
+    created_at,
+    updated_at
+FROM usuarios;
+
+INSERT INTO
+    nuevamedicos (
+        colegiado,
+        nombre,
+        dpi,
+        especialidad,
+        pasaporte,
+        sexo,
+        created_at,
+        updated_at
+    )
+SELECT
+    colegiado,
+    name AS nombre,
+    dpi,
+    especialidad,
+    pasaporte,
+    sexo,
+    created_at,
+    updated_at
+FROM medicos;
+
+INSERT INTO
+    nuevarn (
         fecha,
         cor,
         ao,
@@ -157,7 +227,6 @@ INSERT INTO
         updated_at
     )
 SELECT
-    id,
     fecha,
     cor,
     ao,
@@ -194,14 +263,18 @@ SELECT
     updated_at
 FROM cons_nac;
 
--- Actualización de madre_id
-UPDATE nueva_rn nr
-JOIN nuevapaciente np ON nr.expediente = np.expediente
+CREATE INDEX idx_nuevarn_expediente ON nuevarn (expediente);
+
+CREATE INDEX idx_union_old_expediente ON union_old (expediente);
+
+CREATE INDEX idx_union_old_exp_madre ON union_old (exp_madre);
+
+UPDATE nuevarn nr
+JOIN union_old np ON nr.expediente = np.expediente
 SET
     nr.madre_id = np.paciente_id;
 
--- Actualización de rn_id
-UPDATE nueva_rn nr
-JOIN nuevapaciente np ON nr.expediente = np.exp_madre
+UPDATE nuevarn nr
+JOIN union_old np ON nr.expediente = np.exp_madre
 SET
     nr.rn_id = np.paciente_id;

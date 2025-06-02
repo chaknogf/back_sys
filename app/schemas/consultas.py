257a2923b6b0265d5aa88051f802e44eaaf0dbe9
chaknogf <chaknogf@gmail.com>
2025-06-02 06:34:38 -0600

    chore: schemas refactory

--- app/schemas/consultas.py
+++ app/schemas/consultas.py
@@ -1,45 +1,63 @@
 from typing import Optional, Dict, Any, List
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 from datetime import date, time, datetime
 
 
-class ciclo(BaseModel):
+class Ciclo(BaseModel):
     clave: str
     valor: str
 
-class indicador(BaseModel):
+class Indicador(BaseModel):
     clave: str
     valor: str
     
-class signos_vitales(BaseModel):
+class Signos_vitales(BaseModel):
     clave: str
     valor: str
     
-class ansigmas(BaseModel):
+class Ansigmas(BaseModel):
     clave: str
     valor: str
     
-class antecedentes(BaseModel):
+class Antecedentes(BaseModel):
     clave: str
     valor: str
     
-class ordenes(BaseModel):
+class Ordenes(BaseModel):
     clave: str
     valor: str
-class estudios(BaseModel):
-    clave: str
-    valor: str
-    
-class detalle_clinico(BaseModel):
+class Estudios(BaseModel):
     clave: str
     valor: str
     
-class sistema(BaseModel):
+class Detalle_clinico(BaseModel):
+    clave: str
+    valor: str
+    
+class Sistema(BaseModel):
     usuario: str
     accion: str
     fecha: str
 
 class ConsultaBase(BaseModel):
+    paciente_id: int
+    tipo_consulta: Optional[int]
+    especialidad: Optional[int]
+    servicio: Optional[int]
+    documento: Optional[str]
+    fecha_consulta: Optional[date]
+    hora_consulta: Optional[time]
+    ciclo: Optional[Dict[str, Ciclo]] = Field(default=None)
+    indicadores: Optional[Dict[str, Indicador]] = Field(default=None)
+    detalle_clinico: Optional[Dict[str, Detalle_clinico]] = Field(default=None)
+    sistema: Optional[Dict[str,Sistema]] = Field(default=None)
+    signos_vitales: Optional[Dict[str, Signos_vitales]] = Field(default=None)
+    ansigmas: Optional[Dict[str, Ansigmas]] = Field(default=None)
+    antecedentes: Optional[Dict[str, Antecedentes]] = Field(default=None)
+    ordenes: Optional[Dict[str, Ordenes]] = Field(default=None)
+    estudios: Optional[Dict[str, Estudios]] = Field(default=None)
+    
+class ConsultaUpdate(BaseModel):
     id: Optional[int] = None
     paciente_id: int
     tipo_consulta: Optional[int]
@@ -48,15 +66,15 @@ class ConsultaBase(BaseModel):
     documento: Optional[str]
     fecha_consulta: Optional[date]
     hora_consulta: Optional[time]
-    ciclo: Optional[List[ciclo]]
-    indicadores: Optional[List[indicador]]
-    detalle_clinico: Optional[List[detalle_clinico]]
-    sistema: Optional[List[sistema]]
-    signos_vitales: Optional[List[signos_vitales]]
-    ansigmas: Optional[List[ansigmas]]
-    antecedentes: Optional[List[antecedentes]]
-    ordenes: Optional[List[ordenes]]
-    estudios: Optional[List[estudios]]
+    ciclo: Optional[Dict[str, Ciclo]] = Field(default=None)
+    indicadores: Optional[Dict[str, Indicador]] = Field(default=None)
+    detalle_clinico: Optional[Dict[str, Detalle_clinico]] = Field(default=None)
+    sistema: Optional[Dict[str,Sistema]] = Field(default=None)
+    signos_vitales: Optional[Dict[str, Signos_vitales]] = Field(default=None)
+    ansigmas: Optional[Dict[str, Ansigmas]] = Field(default=None)
+    antecedentes: Optional[Dict[str, Antecedentes]] = Field(default=None)
+    ordenes: Optional[Dict[str, Ordenes]] = Field(default=None)
+    estudios: Optional[Dict[str, Estudios]] = Field(default=None)
 
 class ConsultaCreate(ConsultaBase):
     pass

8f29e76999a8b01075e791933af064c6754ba8e5
chaknogf <chaknogf@gmail.com>
2025-09-23 18:25:55 -0600

    chore: rediseño consultas

--- app/schemas/consultas.py
+++ app/schemas/consultas.py
@@ -18,6 +18,8 @@ class Ciclo(BaseModel):
     estado: Optional[str] = None
     registro: Optional[str] = None
     usuario: Optional[str] = None
+    especialidad: Optional[str] = None
+    servicio: Optional[str] = None
     detalle_clinicos: Optional[Dict] = None
     sistema: Optional[Dict] = None
     signos_vitales: Optional[Dict] = None

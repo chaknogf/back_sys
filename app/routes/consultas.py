c3083ac190fc4368ea43513a27773c1ac398116e
chaknogf <chaknogf@gmail.com>
2025-10-07 02:07:53 -0600

    debuug: filtros

--- app/routes/consultas.py
+++ app/routes/consultas.py
@@ -63,10 +63,11 @@ async def get_consultas(
             
         if identificador:
             query = query.filter(
-                (VistaConsultasModel.cui.cast(String).ilike(f"%{identificador}%")) |
-                (VistaConsultasModel.expediente.ilike(f"%{identificador}%")) |
+                (VistaConsultasModel.cui == identificador) | (VistaConsultasModel.cui.cast(String).ilike(f"%{identificador}%")) |
+                (VistaConsultasModel.expediente ==  identificador) |
                 (VistaConsultasModel.pasaporte.ilike(f"%{identificador}%")) |
-                (VistaConsultasModel.otro_id.ilike(f"%{identificador}%"))
+                (VistaConsultasModel.otro_id.ilike(f"%{identificador}%")) |
+                (VistaConsultasModel.documento == identificador) 
             )
         # === Filtro de fecha consulta (si se envía como string) ===
         if fecha_consulta:

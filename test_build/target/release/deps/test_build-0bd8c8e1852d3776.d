0916f0bf14d58507f9e2185cb38e3b3c545e1db0
chaknogf <chaknogf@gmail.com>
2025-11-27 12:14:25 -0600

    chore: poetry update

---
+++ test_build/target/release/deps/test_build-0bd8c8e1852d3776.d
@@ -0,0 +1,5 @@
+/Users/macbookairm2/Project/hosprojects/back_sys/test_build/target/release/deps/test_build-0bd8c8e1852d3776.d: src/main.rs
+
+/Users/macbookairm2/Project/hosprojects/back_sys/test_build/target/release/deps/test_build-0bd8c8e1852d3776: src/main.rs
+
+src/main.rs:

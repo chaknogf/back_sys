47098b04d97ed02479c64c9c7a907f054d9d000a
chaknogf <chaknogf@gmail.com>
2026-01-19 12:35:42 -0600

    feat: crea neonatos

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from app.routes.user import router as user
 from app.routes.pacientes import router as pacientes
+from app.routes.recienNacido import router as recienNacido
 from app.routes.consultas import router as consultas
 from app.routes.eventos import router as eventos
 from app.routes.expediente import router as expediente
@@ -63,6 +64,7 @@ app.add_middleware(
 app.include_router(login)
 app.include_router(user)
 app.include_router(pacientes)
+app.include_router(recienNacido)
 app.include_router(expediente)
 app.include_router(consultas)
 app.include_router(eventos)
